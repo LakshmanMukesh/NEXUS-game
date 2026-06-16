c90351226cbf3636b4244429827fc23a80202cff
SirThamizhan <kk1416307@gmail.com>
2026-06-16 19:14:20 +0530

    Pre-alpha milestone: player movement and world setup

---
+++ Scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+func _physics_process(delta):
+	player_movement(delta)
+
+func player_movement(delta):
+	if Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = 0
+		velocity.y = SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		velocity.y = 0
+	else:
+		velocity.x=0
+		velocity.y=0
+	move_and_slide()
